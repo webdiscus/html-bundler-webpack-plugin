288cb2056ec7bb3ad9aca3302c0a1b1f948889a1
biodiscus <biodiscus@gmail.com>
2023-07-26 11:16:18 +0200

    style: clean up code

--- src/Loader/cssLoader.js
+++ src/Loader/cssLoader.js
@@ -21,46 +21,8 @@ const loader = function (content) {
 
 /**
  * @this {import("webpack").LoaderContext<LoaderOptions>}
- * @param {string} request
+ * @param {string} remaining
  */
-const pitchLoader2 = function (request) {
-  const loaderContext = this;
-  const { context, resource, resourcePath, entryName, entryId, entryData, _module: module } = loaderContext;
-  const options = loaderContext.getOptions() || {};
-  const callback = loaderContext.async();
-
-  // create a unique request different from the original to avoid cyclic loading of the same style file
-  request += (resource.includes('?') ? '&' : '?') + 'HTMLBundlerCSSLoader';
-
-  loaderContext.importModule(
-    `${resourcePath}.webpack[javascript/auto]!=!!!${request}`,
-    {
-      layer: options.layer,
-      publicPath: urlPathPrefix,
-      baseUri,
-    },
-    /**
-     * @param {Error | null | undefined} error
-     * @param {object} exports
-     */
-    (error, exports) => {
-      if (error) {
-        // TODO: display plugin error
-        callback(error);
-        return;
-      }
-
-      // defaults, the css-loader option `esModule` is `true`
-      const esModule = exports.default != null;
-
-      module._cssSource = esModule ? exports.default : exports;
-      Collection.setImportStyleEsModule(esModule);
-
-      callback(null, '/* extracted by HTMLBundler CSSLoader */ export {};');
-    }
-  );
-};
-
 const pitchLoader = async function (remaining) {
   const { resource, resourcePath, _module: module } = this;
   const options = this.getOptions() || {};
